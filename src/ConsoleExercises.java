4ac494232ac9df39dc963ca73f60e78a918ad1f0
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2023-02-09 16:11:25 -0600

    starting Java

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class ConsoleExercises
+{
+    public static void main(String[] args)
+    {
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %s %n", pi);
+//
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+
+////        System.out.print("Enter something: ");
+////        int userInt = scanner.nextInt();
+////        System.out.println("You entered \"" + userInt + "\"");
+//
+//        System.out.print("Enter three words: ");
+//        String threeWords = scanner.nextLine();
+//        System.out.println("Your three words are \"" + threeWords + "\"");
+
+        System.out.print("Enter width: ");
+        int width = scanner.nextInt();
+
+        System.out.print("Enter length: ");
+        int length = scanner.nextInt();
+
+        double roomWidth = width;
+        double roomLength = length;
+
+        double roomArea = roomLength * roomWidth;
+        double roomPer = roomLength + roomWidth;
+
+        System.out.print("Enter height: ");
+        int height = scanner.nextInt();
+
+        double roomHeight = height;
+
+        double vol = roomArea * roomHeight;
+
+//        System.out.println(roomArea);
+//        System.out.println(roomPer);
+        System.out.printf("The area of the room is %.2f and the perimeter is %.2f %n", roomArea, roomPer);
+        System.out.printf("The volume of the room is: %.2f", vol);
+
+
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,35 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        int myFavoriteNumber = 8;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Hurricane Katrina? More like Hurricane Tortilla";
+        System.out.println(myString);
+
+        float myNumber = 3.14F;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+
+
+
+    }
+
+}
