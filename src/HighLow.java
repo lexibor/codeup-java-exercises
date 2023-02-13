40a216de41c74a0d3e0b95c8b168ef6ad8ef08f4
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2023-02-13 14:50:53 -0600

    spaced out lines

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class HighLow
@@ -11,10 +10,7 @@ public class HighLow
         Scanner scanner = new Scanner(System.in);
         int userInt = Integer.parseInt(scanner.nextLine());
 
-            System.out.println(highOrLow(userInt, rand));
-
-
-
+        System.out.println(highOrLow(userInt, rand));
 
     }
 
@@ -23,20 +19,25 @@ public class HighLow
     {
         count++;
         Scanner scanner = new Scanner(System.in);
+
             if(num > rand)
             {
                 System.out.println();
+
                 System.out.println("NUMBER OF GUESSES: " + count);
                 System.out.print("LOWER: ");
                 int userInt = Integer.parseInt(scanner.nextLine());
+
                 return highOrLow(userInt, rand);
             }
             else if(num < rand)
             {
                 System.out.println();
+
                 System.out.println("NUMBER OF GUESSES: " + count);
                 System.out.print("HIGHER: ");
                 int userInt = Integer.parseInt(scanner.nextLine());
+
                 return highOrLow(userInt, rand);
             }
             return "GUESSED THE NUMBER!";
